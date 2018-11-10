6bfb612f6f79e41e4869657d195cd8b43185c5e6
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2018-11-10 19:30:39 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kadir Uludağ on 10.11.2018.
+//  Copyright © 2018 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
